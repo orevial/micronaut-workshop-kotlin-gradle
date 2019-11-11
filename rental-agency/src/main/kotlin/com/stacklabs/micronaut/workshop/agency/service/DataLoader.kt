2157df9d9c5ae1d96ebe40a3a7622c6e94bf1e2b
Olivier Revial <olivier.revial@gmail.com>
2019-11-11 17:29:46 +0100

    feat(steps): add step 9: add configuration for initial cars loading

--- rental-agency/src/main/kotlin/com/stacklabs/micronaut/workshop/agency/service/DataLoader.kt
+++ rental-agency/src/main/kotlin/com/stacklabs/micronaut/workshop/agency/service/DataLoader.kt
@@ -1,8 +1,8 @@
 package com.stacklabs.micronaut.workshop.agency.service
 
+import com.stacklabs.micronaut.workshop.agency.config.AppConfiguration
 import com.stacklabs.micronaut.workshop.agency.domain.Car
 import com.stacklabs.micronaut.workshop.agency.persistence.CarRepository
-import com.stacklabs.micronaut.workshop.api.v1.model.CarCategory
 import io.micronaut.context.annotation.Requires
 import io.micronaut.context.env.Environment
 import io.micronaut.context.event.ApplicationEventListener
@@ -13,19 +13,17 @@ import javax.inject.Singleton
 
 @Singleton
 @Requires(notEnv = [Environment.TEST])
-class DataLoader(var repository: CarRepository) : ApplicationEventListener<ServiceStartedEvent> {
+class DataLoader(var repository: CarRepository, var appConfiguration: AppConfiguration) : ApplicationEventListener<ServiceStartedEvent> {
 
     companion object {
         val LOG: Logger = LoggerFactory.getLogger(DataLoader::class.java)
     }
 
     override fun onApplicationEvent(event: ServiceStartedEvent?) {
-        listOf(
-            Car(registration = "AA-982-BB", brand = "Tesla", model = "Model S", category = CarCategory.COUPE),
-            Car(registration = "AA-123-BB", brand = "Ford", model = "Fiesta", category = CarCategory.COMPACT_CAR)
-        )
-            .onEach { LOG.info("Going to save car with registration ${it.registration}...") }
-            .forEach { repository.save(it).ifPresentOrElse(
+        appConfiguration.initialCars
+            ?.map { Car(registration = it.registration, category = it.category, model = it.model, brand = it.brand) }
+            ?.onEach { LOG.info("Going to save car with registration ${it.registration}...") }
+            ?.forEach { repository.save(it).ifPresentOrElse(
                 { LOG.info("Successfuly saved car with registration ${it.registration}...") },
                 { LOG.warn("Could not save car with registration ${it.registration}...") }
             )}
--- rental-agency/src/test/kotlin/com/stacklabs/micronaut/workshop/agency/DataLoaderTest.kt
+++ rental-agency/src/test/kotlin/com/stacklabs/micronaut/workshop/agency/DataLoaderTest.kt
@@ -1,7 +1,9 @@
 package com.stacklabs.micronaut.workshop.agency
 
+import com.stacklabs.micronaut.workshop.agency.config.AppConfiguration
 import com.stacklabs.micronaut.workshop.agency.persistence.CarRepository
 import com.stacklabs.micronaut.workshop.agency.service.DataLoader
+import com.stacklabs.micronaut.workshop.api.v1.model.CarCategory
 import io.micronaut.test.annotation.MicronautTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
@@ -9,7 +11,14 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 @MicronautTest
-class DataLoaderTest(private val carRepository: CarRepository) {
+class DataLoaderTest(private val carRepository: CarRepository, private val config: AppConfiguration) {
+
+    private val expectedCars = mapOf(
+        Pair(0, CarCategory.COUPE),
+        Pair(1, CarCategory.CONVERTIBLE),
+        Pair(2, CarCategory.MINIVAN),
+        Pair(3, CarCategory.STATION_WAGON)
+    )
 
     @BeforeEach
     fun setup() {
@@ -24,12 +33,21 @@ class DataLoaderTest(private val carRepository: CarRepository) {
     @Test
     fun testDataLoader() {
         // Given
-        val dataLoader = DataLoader(carRepository)
+        val dataLoader = DataLoader(carRepository, config)
 
         // When
         dataLoader.onApplicationEvent(null)
 
         // Then
-        assertThat(carRepository.findAll()).hasSize(2)
+        val cars = carRepository.findAll()
+        assertThat(cars).hasSameSizeAs(expectedCars.entries)
+
+        expectedCars.entries
+            .forEach {
+                assertThat(cars[it.key].registration).isEqualTo("TEST-${it.key}")
+                assertThat(cars[it.key].brand).isEqualTo("Brand${it.key}")
+                assertThat(cars[it.key].model).isEqualTo("Model${it.key}")
+                assertThat(cars[it.key].category).isEqualTo(it.value)
+            }
     }
 }
