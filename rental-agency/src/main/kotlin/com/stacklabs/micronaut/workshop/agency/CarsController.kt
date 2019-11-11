f0dfa0b970a97e5037bd299ad18735764629b582
Olivier Revial <olivier.revial@gmail.com>
2019-11-11 16:35:05 +0100

    feat(steps): add step 5: add CarsController with test

--- rental-agency/src/main/kotlin/com/stacklabs/micronaut/workshop/agency/CarsController.kt
+++ rental-agency/src/main/kotlin/com/stacklabs/micronaut/workshop/agency/CarsController.kt
@@ -2,13 +2,10 @@ package com.stacklabs.micronaut.workshop.agency
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.HttpStatus
 
 @Controller("/cars")
 class CarsController {
 
     @Get("/")
-    fun index(): HttpStatus {
-        return HttpStatus.OK
-    }
+    fun index() = "Hello Micronaut!"
 }
