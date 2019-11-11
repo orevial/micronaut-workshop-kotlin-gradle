6a2200fcef6001f53ba17c6376b76d9298d96942
Olivier Revial <olivier.revial@gmail.com>
2019-11-11 16:35:05 +0100

    feat(steps): add step 5: add CarsController with test

--- rental-agency/src/test/kotlin/com/stacklabs/micronaut/workshop/agency/CarsControllerTest.kt
+++ rental-agency/src/test/kotlin/com/stacklabs/micronaut/workshop/agency/CarsControllerTest.kt
@@ -1,4 +1,17 @@
 package com.stacklabs.micronaut.workshop.agency
 
-class CarsControllerTest {
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.test.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+
+@MicronautTest
+class CarsControllerTest(private val embeddedServer: EmbeddedServer) {
+
+    @Test
+    fun testHelloMicronaut() =
+        embeddedServer.applicationContext.createBean(RxHttpClient::class.java, embeddedServer.url)
+            .use { client -> assertEquals("Hello Micronaut!", client.toBlocking().exchange("/cars", String::class.java).body()) }
 }
